ad1739f2df62513df6f6caad19fd2b912d82a23c
Maximiliano Marchant <lab.mm.spa@gmail.com>
2024-03-23 12:14:34 -0300

    Antes de custom provider Axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
   
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
-    
-    data.results.forEach(({ name, url }) => {
 
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    
+    data.results.forEach(({ name, url }) => { // De esta forma se agregan individualmente a la base de datos
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
 
+      //const pokemon = await this.pokemonModel.create( {name, no} )
+      pokemonToInsert.push({name,no}); // Arreglo de pokemones
+    });
 
-      console.log({ name, no});
-
-
-    })
+    await this.pokemonModel.insertMany(pokemonToInsert);
     
-    return data.results;
+    return 'Seed Executed';
   }
+
+
+  // VERSION SUBIDA INIDIVIDUAL
+  // async executeSeed() {
+
+  //   await this.pokemonModel.deleteMany({});
+
+  //   const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+  //   const insertPromisesArray = [];
+
+    
+  //   data.results.forEach( ({ name, url }) => { // De esta forma se agregan individualmente a la base de datos
+  //                                             // No es eficiente en lotes de información 
+
+  //     const segments = url.split('/');
+  //     const no = +segments[ segments.length - 2 ];
+
+
+  //     //const pokemon = await this.pokemonModel.create( {name, no} )
+
+  //     insertPromisesArray.push(
+  //       this.pokemonModel.create({name,no})
+  //     )
+  //   });
+
+  //   const newArray = await Promise.all(insertPromisesArray);
+    
+  //   return 'Seed Executed';
+  // }
 
 }
